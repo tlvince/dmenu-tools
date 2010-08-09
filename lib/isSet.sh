b8b2329bd42cfecbbc90bc611997ed50479d45c4
Tom Vincent <git@tlvince.com>
2010-08-09 12:29:30 +0100

    Make isSet POSIX compatible

--- lib/isSet.sh
+++ lib/isSet.sh
@@ -1,12 +1,11 @@
 ##
 # If the given variable is not set, prompt to set it.
 #
-# Note: ${!1} is a bash-ism.
-#
 # @param $1 string  The name of a variable.
 # @param $2 string  An arbitrary prompt, corresponding to $1.
 #
 isSet()
 {
-    [[ -z ${!1} ]] && read -p "$2" $1 && isSet "$@"
+    eval tmp=\$$1
+    [[ -z $tmp ]] && read -p "$2" $1 && isSet "$@"
 }
