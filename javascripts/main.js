71cfa91018f0f32f35a1bb43e18e3d99c49cac92
Tom Vincent <git@tlvince.com>
2013-03-16 17:09:35 +0800

    Populate the readme with js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+$(function() {
+    $.getJSON("https://api.github.com/repos/tlvince/dmenu-tools/readme", function(data) {
+    var readme = data.content.replace(/\n/g, '');
+    readme = window.atob(readme);
+    var html = new Markdown.Converter().makeHtml(readme);
+    $('#main_content').replaceWith(html);
+  });
+});
